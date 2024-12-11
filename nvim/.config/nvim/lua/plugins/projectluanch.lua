df333a703d960ff206f89668fc36caa271580194
AqeAyen <kervgamingph@gmail.com>
2024-12-11 18:00:36 +0800

    added some shit

--- nvim/.config/nvim/lua/plugins/projectluanch.lua
+++ nvim/.config/nvim/lua/plugins/projectluanch.lua
@@ -9,6 +9,20 @@ return {
       -- open the floating window terminal viewer
       vim.keymap.set('n', "<leader>pf", projectlaunch.toggle_float,
         { noremap = true, expr = false, buffer = false, desc = "Open the floating window" })
+
+      -- open the split window terminal viewer
+      vim.keymap.set('n', "<leader>ps", projectlaunch.toggle_split,
+        { noremap = true, expr = false, buffer = false, desc = "open the split window terminal" })
+
+      -- show the next or previous terminals in the open viewer
+      vim.keymap.set('n', "<leader>pn", projectlaunch.show_next,
+        { noremap = true, expr = false, buffer = false, desc = "show the next terminal" })
+      vim.keymap.set('n', "<leader>pm", projectlaunch.show_prev,
+        { noremap = true, expr = false, buffer = false, desc = "show the previous terminal" })
+
+      -- restart the command running in the currently open split terminal
+      vim.keymap.set('n', "<leader>pr", projectlaunch.restart_command_in_split,
+        { noremap = true, expr = false, buffer = false, desc = "restart the command in the split terminal" })
     end
   }
 }
