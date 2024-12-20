c6462fe97bebbf3697d81cc1d70f3a6cb9e3190c
AqeAyen <kervgamingph@gmail.com>
2024-12-20 15:57:18 +0800

    better catpuccin

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -26,6 +26,38 @@ local splash = {
 math.randomseed()
 require("notify")(splash[math.random(1, #splash)])
 require("provider.clipboard.health").check()
+require("catppuccin").setup {
+  color_overrides = {
+    all = {
+      rosewater = "#f5e0dc",
+      flamingo = "#f2cdcd",
+      pink = "#ffbbed",
+      mauve = "#cba6f7",
+      red = "#f38ba8",
+      maroon = "#eba0ac",
+      peach = "#efb896",
+      yellow = "#f9e2af",
+      green = "#a6e3a1",
+      teal = "#94e2d5",
+      sky = "#89dceb",
+      sapphire = "#74c7ec",
+      blue = "#89b4fa",
+      lavender = "#b9c0ea",
+      text = "#cdd6f4",
+      subtext1 = "#bac2de",
+      subtext0 = "#a6adc8",
+      overlay2 = "#9399b2",
+      overlay1 = "#7f849c",
+      overlay0 = "#6c7086",
+      surface2 = "#585b70",
+      surface1 = "#45475a",
+      surface0 = "#313244",
+      base = "#1e1e2e",
+      mantle = "#181825",
+      crust = "#11111b",
+    },
+  },
+}
 -- vim.lsp.config.clangd {
 --   cmd = {
 --     "clangd",
