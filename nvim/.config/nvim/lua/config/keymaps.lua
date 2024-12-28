e216069105ec5c54fdac07db582c387471444b06
AqeAyen <kervgamingph@gmail.com>
2024-12-28 11:17:56 +0800

    added a bunch of new plugin + custom options
    Here I added two new plugins and also a custom options you can do
    leadercC to compile your file base on a command yoi gave it similar to
    emacs

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -12,7 +12,10 @@ vim.opt.mouse = ""
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 vim.keymap.set("n", "<leader>fF", "<cmd>Telescope frecency<cr>", { desc = "telescope frecency" })
 vim.keymap.set("n", "<leader>uT", "<cmd>UndotreeToggle<cr>", { desc = "undotree toggle" })
-vim.keymap.set('n', '<leader>sc', '<cmd>lua require("cheat-sh").search()<cr>')
-vim.keymap.set('n', '<leader>sx', '<cmd>lua require("cheat-sh").get_cursor_word(true)<cr>')
+vim.keymap.set("n", "<leader>sc", '<cmd>lua require("cheat-sh").search()<cr>')
+vim.keymap.set("n", "<leader>sx", '<cmd>lua require("cheat-sh").get_cursor_word(true)<cr>')
 -- get_cursor_word(true) will open the search window so you can edit the query
 -- without or set to false will run the search on cursor word
+vim.keymap.set("n", "<leader>gg", function()
+  require("lazygit-confirm").confirm()
+end, { noremap = true })
--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,3 +1,16 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+-- Function to prompt the user for a compile command and execute it in a terminal
+local function compile()
+  vim.ui.input({ prompt = "Enter compile command: " }, function(input)
+    if input and #input > 0 then
+      vim.cmd("split | terminal " .. input)
+    else
+      vim.notify("No compile command provided.", vim.log.levels.WARN)
+    end
+  end)
+end
+
+-- Set up the keybinding directly in LazyVim's configuration
+vim.keymap.set("n", "<leader>cC", compile, { noremap = true, silent = true, desc = "Compile project" })
